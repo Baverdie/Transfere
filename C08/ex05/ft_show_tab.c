5dfd16e503f84947d41d53181d1bb609c32404d9
Fondit <bastien.verdiervaissiere@gmail.com>
2023-08-02 12:53:45 +0200

    c08

--- C08/ex05/ft_show_tab.c
+++ C08/ex05/ft_show_tab.c
@@ -12,9 +12,59 @@
 
 #include "ft_stock_str.h"
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	long	n;
+
+	n = nb;
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n *= -1;
+	}
+	if (n >= 0 && n <= 9)
+		ft_putchar(n + 48);
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putchar(n % 10 + 48);
+	}
+}
+
 void	ft_show_tab(struct s_stock_str *par)
 {
-	write(1, tab[i].str, 1);
-	write(1, tab[i].size, 1);
-	write(1, tab[i].copy, 1);
+	int		i;
+	char	*str;
+	int		size;
+
+	i = 0;
+	while (par[i + 1].str != 0)
+	{
+		str = par[i].str;
+		size = par[i].size;
+		ft_putstr(str);
+		ft_putchar('\n');
+		ft_putnbr(size);
+		ft_putchar('\n');
+		ft_putstr(par[i].copy);
+		ft_putchar('\n');
+		i++;
+	}
 }
